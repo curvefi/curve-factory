6c0e94c4ea6b8b1bec94b6bb630e20cad65b11f0
Edward Amor <edward.amor3@gmail.com>
2021-08-02 11:56:59 -0400

    fix(test): mass exit claim rewards

--- tests/gauge/test_mass_exit_reward_claim.py
+++ tests/gauge/test_mass_exit_reward_claim.py
@@ -1,27 +1,23 @@
 import math
 
 import pytest
-from brownie import ZERO_ADDRESS
 
 REWARD = 10 ** 20
 WEEK = 7 * 86400
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(autouse=True)
 def initial_setup(
+    add_initial_liquidity,
     alice,
     accounts,
     chain,
     coin_reward,
-    reward_contract,
-    token,
     swap,
     gauge,
     gauge_controller,
-    minter,
 ):
     # gauge setup
-    token.set_minter(minter, {"from": alice})
     gauge_controller.add_type(b"Liquidity", 10 ** 10, {"from": alice})
     gauge_controller.add_gauge(gauge, 0, 0, {"from": alice})
 
@@ -32,28 +28,21 @@ def initial_setup(
         gauge.deposit(10 ** 18, acct, {"from": alice})
 
     # add rewards
-    sigs = [
-        reward_contract.stake.signature[2:],
-        reward_contract.withdraw.signature[2:],
-        reward_contract.getReward.signature[2:],
-    ]
-    sigs = f"0x{sigs[0]}{sigs[1]}{sigs[2]}{'00' * 20}"
-
-    gauge.set_rewards(reward_contract, sigs, [coin_reward] + [ZERO_ADDRESS] * 7, {"from": alice})
+    gauge.add_reward(coin_reward, alice, {"from": alice})
 
     # fund rewards
-    coin_reward._mint_for_testing(reward_contract, REWARD)
-    reward_contract.notifyRewardAmount(REWARD, {"from": alice})
+    coin_reward._mint_for_testing(alice, REWARD, {"from": alice})
+    coin_reward.approve(gauge, 2 ** 256 - 1, {"from": alice})
+    gauge.deposit_reward_token(coin_reward, REWARD, {"from": alice})
 
     # sleep half way through the reward period
     chain.sleep(WEEK)
 
 
-def test_mass_withdraw_claim_rewards(accounts, gauge, coin_reward, swap):
+def test_mass_withdraw_claim_rewards(accounts, gauge, coin_reward):
     for account in accounts[:10]:
         gauge.withdraw(gauge.balanceOf(account), {"from": account})
         assert gauge.claimed_reward(account, coin_reward) == 0
-        assert gauge.claimable_reward_write.call(account, coin_reward) > 0
 
     for account in accounts[:10]:
         gauge.claim_rewards({"from": account})

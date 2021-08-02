1771fc679cd6c2d4662bc7eeadeae91589a90642
Edward Amor <edward.amor3@gmail.com>
2021-08-02 11:39:05 -0400

    fix(test): set default reward receiver

--- tests/gauge/test_set_rewards_receiver.py
+++ tests/gauge/test_set_rewards_receiver.py
@@ -1,5 +1,4 @@
 import pytest
-from brownie import ZERO_ADDRESS, compile_source
 from pytest import approx
 
 REWARD = 10 ** 20
@@ -7,45 +6,16 @@ WEEK = 7 * 86400
 LP_AMOUNT = 10 ** 18
 
 
-code = """
-# @version 0.2.7
+@pytest.fixture(autouse=True)
+def initial_setup(
+    alice, add_initial_liquidity, bob, chain, gauge, coin_reward, coin_a, coin_b, swap
+):
 
-from vyper.interfaces import ERC20
-
-first: address
-second: address
-
-@external
-def __init__(_first: address, _second: address):
-    self.first = _first
-    self.second = _second
-
-@external
-def claim_tokens() -> bool:
-    ERC20(self.first).transfer(msg.sender, ERC20(self.first).balanceOf(self) / 2)
-    ERC20(self.second).transfer(msg.sender, ERC20(self.second).balanceOf(self) / 2)
-
-    return True
-"""
-
-
-@pytest.fixture(scope="module")
-def reward_contract(alice, coin_a, coin_b):
-    contract = compile_source(code).Vyper.deploy(coin_a, coin_b, {"from": alice})
-    coin_a._mint_for_testing(contract, REWARD * 2)
-    coin_b._mint_for_testing(contract, REWARD * 2)
-
-    yield contract
-
-
-@pytest.fixture(scope="module", autouse=True)
-def initial_setup(alice, bob, chain, gauge, reward_contract, coin_reward, coin_a, coin_b, swap):
-
-    sigs = f"0x{'00' * 4}{'00' * 4}{reward_contract.claim_tokens.signature[2:]}{'00' * 20}"
-
-    gauge.set_rewards(
-        reward_contract, sigs, [coin_a, coin_reward, coin_b] + [ZERO_ADDRESS] * 5, {"from": alice}
-    )
+    for coin in [coin_reward, coin_a, coin_b]:
+        gauge.add_reward(coin, alice, {"from": alice})
+        coin._mint_for_testing(alice, REWARD, {"from": alice})
+        coin.approve(gauge, 2 ** 256 - 1, {"from": alice})
+        gauge.deposit_reward_token(coin, REWARD, {"from": alice})
 
     # Deposit
     swap.transfer(bob, LP_AMOUNT, {"from": alice})

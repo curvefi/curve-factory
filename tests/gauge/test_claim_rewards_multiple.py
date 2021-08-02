b41262cacd7a2a6adc60018d1cde8bed8615d62e
Edward Amor <edward.amor3@gmail.com>
2021-08-02 12:42:31 -0400

    fix(test): claim rewards multiple

--- tests/gauge/test_claim_rewards_multiple.py
+++ tests/gauge/test_claim_rewards_multiple.py
@@ -2,7 +2,6 @@ import math
 
 import brownie
 import pytest
-from brownie import ZERO_ADDRESS, compile_source
 from pytest import approx
 
 REWARD = 10 ** 20
@@ -10,50 +9,19 @@ WEEK = 7 * 86400
 LP_AMOUNT = 10 ** 18
 
 
-code = """
-# @version 0.2.7
-
-from vyper.interfaces import ERC20
-
-first: address
-second: address
-
-@external
-def __init__(_first: address, _second: address):
-    self.first = _first
-    self.second = _second
-
-@external
-def claim_tokens() -> bool:
-    ERC20(self.first).transfer(msg.sender, ERC20(self.first).balanceOf(self) / 2)
-    ERC20(self.second).transfer(msg.sender, ERC20(self.second).balanceOf(self) / 2)
-
-    return True
-"""
-
-
-@pytest.fixture(scope="module")
-def reward_contract(alice, coin_a, coin_b):
-    contract = compile_source(code).Vyper.deploy(coin_a, coin_b, {"from": alice})
-    coin_a._mint_for_testing(contract, REWARD * 2)
-    coin_b._mint_for_testing(contract, REWARD * 2)
-
-    yield contract
-
-
-@pytest.fixture(scope="module", autouse=True)
-def initial_setup(alice, bob, chain, gauge, reward_contract, coin_reward, coin_a, coin_b, swap):
-
-    sigs = f"0x{'00' * 4}{'00' * 4}{reward_contract.claim_tokens.signature[2:]}{'00' * 20}"
-
-    gauge.set_rewards(
-        reward_contract, sigs, [coin_a, coin_reward, coin_b] + [ZERO_ADDRESS] * 5, {"from": alice}
-    )
+@pytest.fixture(autouse=True)
+def initial_setup(add_initial_liquidity, alice, bob, gauge, coin_reward, coin_a, coin_b, swap):
 
     # Deposit
     swap.transfer(bob, LP_AMOUNT, {"from": alice})
     swap.approve(gauge, LP_AMOUNT, {"from": bob})
     gauge.deposit(LP_AMOUNT, {"from": bob})
+
+    for coin in [coin_reward, coin_a, coin_b]:
+        coin._mint_for_testing(alice, REWARD, {"from": alice})
+        coin.approve(gauge, 2 ** 256 - 1, {"from": alice})
+        gauge.add_reward(coin, alice, {"from": alice})
+        gauge.deposit_reward_token(coin, REWARD, {"from": alice})
 
 
 def test_claim_one_lp(bob, chain, gauge, coin_a, coin_b):
@@ -106,16 +74,13 @@ def test_claim_for_other_no_reward(bob, charlie, chain, gauge, coin_a, coin_b):
     assert coin_b.balanceOf(charlie) == 0
 
 
-def test_claim_two_lp(
-    alice, bob, chain, gauge, swap, coin_a, coin_b, reward_contract, no_call_coverage
-):
+@pytest.mark.no_call_coverage
+def test_claim_two_lp(alice, bob, chain, gauge, swap, coin_a, coin_b):
 
+    chain.sleep(86400)
     # Deposit
     swap.approve(gauge, LP_AMOUNT, {"from": alice})
     gauge.deposit(LP_AMOUNT, {"from": alice})
-
-    coin_a._mint_for_testing(reward_contract, REWARD)
-    coin_b._mint_for_testing(reward_contract, REWARD)
 
     chain.sleep(WEEK)
     chain.mine()
@@ -126,31 +91,7 @@ def test_claim_two_lp(
     for coin in (coin_a, coin_b):
         # Calculate rewards
         assert coin.balanceOf(bob) > coin.balanceOf(alice) > 0
-        assert coin.balanceOf(gauge) == 0
-
-
-def test_claim_duration(bob, chain, gauge, coin_a, coin_b):
-    chain.sleep(86400)
-    gauge.claim_rewards({"from": bob})
-
-    claim_time = gauge.last_claim()
-    claimed = [i.balanceOf(bob) for i in (coin_a, coin_b)]
-
-    assert math.isclose(claim_time, chain[-1].timestamp, abs_tol=1)
-
-    chain.sleep(1801)
-    gauge.claim_rewards({"from": bob})
-
-    assert math.isclose(gauge.last_claim(), claim_time, abs_tol=1)
-    assert claimed == [i.balanceOf(bob) for i in (coin_a, coin_b)]
-
-    chain.sleep(1801)
-    gauge.claim_rewards({"from": bob})
-
-    assert math.isclose(gauge.last_claim(), chain[-1].timestamp, abs_tol=1)
-    assert chain[-1].timestamp > claim_time
-    assert coin_a.balanceOf(bob) > claimed[0]
-    assert coin_b.balanceOf(bob) > claimed[1]
+        assert math.isclose((coin.balanceOf(gauge) / REWARD), 0, abs_tol=10 ** -9)
 
 
 def test_claim_set_alt_receiver(bob, charlie, chain, gauge, coin_a, coin_b):

2d72b85a0a8ca98d5808ead13b23ffd3ad3d557e
Edward Amor <edward.amor3@gmail.com>
2021-08-02 12:46:49 -0400

    fix(test): claim rewards none gauge

--- tests/gauge/test_claim_rewards_none.py
+++ tests/gauge/test_claim_rewards_none.py
@@ -1,24 +1,22 @@
-from brownie import ZERO_ADDRESS
+import pytest
 
 REWARD = 10 ** 20
 WEEK = 7 * 86400
 LP_AMOUNT = 10 ** 18
 
 
-def test_claim_no_deposit(alice, bob, chain, gauge, swap, reward_contract, coin_reward):
+pytestmark = pytest.mark.usefixtures("add_initial_liquidity")
+
+
+def test_claim_no_deposit(alice, bob, chain, gauge, swap, coin_reward):
     # Fund
     swap.approve(gauge, LP_AMOUNT, {"from": alice})
     gauge.deposit(LP_AMOUNT, {"from": alice})
 
-    coin_reward._mint_for_testing(reward_contract, REWARD)
-    reward_contract.notifyRewardAmount(REWARD, {"from": alice})
-
-    gauge.set_rewards(
-        reward_contract,
-        "0xa694fc3a2e1a7d4d3d18b9120000000000000000000000000000000000000000",
-        [coin_reward] + [ZERO_ADDRESS] * 7,
-        {"from": alice},
-    )
+    coin_reward._mint_for_testing(alice, REWARD, {"from": alice})
+    coin_reward.approve(gauge, REWARD, {"from": alice})
+    gauge.add_reward(coin_reward, alice, {"from": alice})
+    gauge.deposit_reward_token(coin_reward, REWARD, {"from": alice})
 
     chain.sleep(WEEK)
 
@@ -27,11 +25,13 @@ def test_claim_no_deposit(alice, bob, chain, gauge, swap, reward_contract, coin_
     assert coin_reward.balanceOf(bob) == 0
 
 
-def test_claim_no_rewards(alice, bob, chain, gauge, swap, reward_contract, coin_reward):
+def test_claim_no_rewards(alice, bob, chain, gauge, swap, coin_reward):
     # Deposit
     swap.transfer(bob, LP_AMOUNT, {"from": alice})
     swap.approve(gauge, LP_AMOUNT, {"from": bob})
     gauge.deposit(LP_AMOUNT, {"from": bob})
+
+    gauge.add_reward(coin_reward, alice, {"from": alice})
 
     chain.sleep(WEEK)
 

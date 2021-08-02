3468b463c7f20534b3c8c97cf41e38af7367f34c
Edward Amor <edward.amor3@gmail.com>
2021-08-02 11:39:21 -0400

    fix(test): gauge transfer tests

--- tests/gauge/test_transfer.py
+++ tests/gauge/test_transfer.py
@@ -3,89 +3,88 @@ import brownie
 import pytest
 
 
-@pytest.fixture(scope="module", autouse=True)
-def setup(accounts, gauge_controller, minter, gauge, token, swap):
-    token.set_minter(minter, {"from": accounts[0]})
+@pytest.fixture(autouse=True)
+def setup(add_initial_liquidity, alice, gauge_controller, minter, gauge, swap):
 
-    gauge_controller.add_type(b"Liquidity", 10 ** 10, {"from": accounts[0]})
-    gauge_controller.add_gauge(gauge, 0, 0, {"from": accounts[0]})
+    gauge_controller.add_type(b"Liquidity", 10 ** 10, {"from": alice})
+    gauge_controller.add_gauge(gauge, 0, 0, {"from": alice})
 
-    swap.approve(gauge, 2 ** 256 - 1, {"from": accounts[0]})
-    gauge.deposit(10 ** 18, {"from": accounts[0]})
+    swap.approve(gauge, 2 ** 256 - 1, {"from": alice})
+    gauge.deposit(10 ** 18, {"from": alice})
 
 
-def test_sender_balance_decreases(accounts, gauge):
-    sender_balance = gauge.balanceOf(accounts[0])
+def test_sender_balance_decreases(alice, bob, gauge):
+    sender_balance = gauge.balanceOf(alice)
     amount = sender_balance // 4
 
-    gauge.transfer(accounts[1], amount, {"from": accounts[0]})
+    gauge.transfer(bob, amount, {"from": alice})
 
-    assert gauge.balanceOf(accounts[0]) == sender_balance - amount
-
-
-def test_receiver_balance_increases(accounts, gauge):
-    receiver_balance = gauge.balanceOf(accounts[1])
-    amount = gauge.balanceOf(accounts[0]) // 4
-
-    gauge.transfer(accounts[1], amount, {"from": accounts[0]})
-
-    assert gauge.balanceOf(accounts[1]) == receiver_balance + amount
+    assert gauge.balanceOf(alice) == sender_balance - amount
 
 
-def test_total_supply_not_affected(accounts, gauge):
+def test_receiver_balance_increases(alice, bob, gauge):
+    receiver_balance = gauge.balanceOf(bob)
+    amount = gauge.balanceOf(alice) // 4
+
+    gauge.transfer(bob, amount, {"from": alice})
+
+    assert gauge.balanceOf(bob) == receiver_balance + amount
+
+
+def test_total_supply_not_affected(alice, bob, gauge):
     total_supply = gauge.totalSupply()
-    amount = gauge.balanceOf(accounts[0])
+    amount = gauge.balanceOf(alice)
 
-    gauge.transfer(accounts[1], amount, {"from": accounts[0]})
+    gauge.transfer(bob, amount, {"from": alice})
 
     assert gauge.totalSupply() == total_supply
 
 
-def test_returns_true(accounts, gauge):
-    amount = gauge.balanceOf(accounts[0])
-    tx = gauge.transfer(accounts[1], amount, {"from": accounts[0]})
+def test_returns_true(alice, bob, gauge):
+    amount = gauge.balanceOf(alice)
+    tx = gauge.transfer(bob, amount, {"from": alice})
 
     assert tx.return_value is True
 
 
-def test_transfer_full_balance(accounts, gauge):
-    amount = gauge.balanceOf(accounts[0])
-    receiver_balance = gauge.balanceOf(accounts[1])
+def test_transfer_full_balance(alice, bob, gauge):
+    amount = gauge.balanceOf(alice)
+    receiver_balance = gauge.balanceOf(bob)
 
-    gauge.transfer(accounts[1], amount, {"from": accounts[0]})
+    gauge.transfer(bob, amount, {"from": alice})
 
-    assert gauge.balanceOf(accounts[0]) == 0
-    assert gauge.balanceOf(accounts[1]) == receiver_balance + amount
-
-
-def test_transfer_zero_tokens(accounts, gauge):
-    sender_balance = gauge.balanceOf(accounts[0])
-    receiver_balance = gauge.balanceOf(accounts[1])
-
-    gauge.transfer(accounts[1], 0, {"from": accounts[0]})
-
-    assert gauge.balanceOf(accounts[0]) == sender_balance
-    assert gauge.balanceOf(accounts[1]) == receiver_balance
+    assert gauge.balanceOf(alice) == 0
+    assert gauge.balanceOf(bob) == receiver_balance + amount
 
 
-def test_transfer_to_self(accounts, gauge):
-    sender_balance = gauge.balanceOf(accounts[0])
+def test_transfer_zero_tokens(alice, bob, gauge):
+    sender_balance = gauge.balanceOf(alice)
+    receiver_balance = gauge.balanceOf(bob)
+
+    gauge.transfer(bob, 0, {"from": alice})
+
+    assert gauge.balanceOf(alice) == sender_balance
+    assert gauge.balanceOf(bob) == receiver_balance
+
+
+def test_transfer_to_self(alice, gauge):
+    sender_balance = gauge.balanceOf(alice)
     amount = sender_balance // 4
 
-    gauge.transfer(accounts[0], amount, {"from": accounts[0]})
+    gauge.transfer(alice, amount, {"from": alice})
 
-    assert gauge.balanceOf(accounts[0]) == sender_balance
+    assert gauge.balanceOf(alice) == sender_balance
 
 
-def test_insufficient_balance(accounts, gauge):
-    balance = gauge.balanceOf(accounts[0])
+def test_insufficient_balance(alice, bob, gauge):
+    balance = gauge.balanceOf(alice)
 
     with brownie.reverts():
-        gauge.transfer(accounts[1], balance + 1, {"from": accounts[0]})
+        gauge.transfer(bob, balance + 1, {"from": alice})
 
 
-def test_transfer_event_fires(accounts, gauge):
-    amount = gauge.balanceOf(accounts[0])
-    tx = gauge.transfer(accounts[1], amount, {"from": accounts[0]})
+def test_transfer_event_fires(alice, bob, gauge):
+    amount = gauge.balanceOf(alice)
+    tx = gauge.transfer(bob, amount, {"from": alice})
 
-    assert tx.events["Transfer"].values() == [accounts[0], accounts[1], amount]
+    assert tx.events["Transfer"].values() == [alice, bob, amount]
